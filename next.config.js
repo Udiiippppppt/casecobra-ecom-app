ca8dd48202d77a275822128853562c7e520624ac
udipt <udipt@672002.com>
2024-09-25 18:06:49 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint:{
-    ignoreDuringBuilds:true,
-  },
-}
-
-
-
-module.exports = nextConfig, {
-  
+module.exports = {
     images: {
       domains: ['utfs.io'], // Add the external image domain here
     },
